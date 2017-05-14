d9e4682989f778c174ee81365f25aa840f3dd5aa
Mark Lopez <m@silvenga.com>
2017-05-14 11:42:25 -0500

    Basic project structure, added rpc models.

---
+++ src/Yals/JsonRpc/Models/RpcId.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Yals.JsonRpc.Models
+{
+    [JsonConverter(typeof(RpcIdJsonConverter))]
+    public class RpcId
+    {
+        public string Value { get; set; }
+
+        public RpcIdKind Kind { get; set; }
+    }
+
+    public enum RpcIdKind
+    {
+        String,
+        Number
+    }
+
+    public class RpcIdJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var id = (RpcId)value;
+
+            switch (id.Kind)
+            {
+                case RpcIdKind.String:
+                    writer.WriteValue(id.Value);
+                    break;
+                case RpcIdKind.Number:
+                    writer.WriteValue(int.Parse(id.Value));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Integer)
+            {
+                return new RpcId
+                {
+                    Value = reader.Value.ToString(),
+                    Kind = RpcIdKind.Number
+                };
+            }
+            if (reader.TokenType == JsonToken.String)
+            {
+                return new RpcId
+                {
+                    Value = reader.Value.ToString(),
+                    Kind = RpcIdKind.String
+                };
+            }
+
+            throw new NotImplementedException("Unknown RPC Id Kind.");
+        }
+    }
+}
---
+++ tests/Yals.Tests/JsonRpc/RpcIdFacts.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+
+using Newtonsoft.Json;
+
+using Ploeh.AutoFixture;
+
+using Xunit;
+
+using Yals.JsonRpc.Models;
+
+namespace Yals.Tests.JsonRpc
+{
+    public class RpcIdFacts
+    {
+        private static readonly Fixture Autofixture = new Fixture();
+
+        [Fact]
+        public void When_reading_id_and_is_a_number_then_correctly_parse()
+        {
+            var numberId = Autofixture.Create<int>();
+            var json = $"{numberId}";
+
+            // Act
+            var result = JsonConvert.DeserializeObject<RpcId>(json);
+
+            // Assert
+            result.Kind.Should().Be(RpcIdKind.Number);
+            result.Value.Should().Be(numberId.ToString());
+        }
+
+        [Fact]
+        public void When_reading_id_and_is_a_string_then_correctly_parse()
+        {
+            var numberId = Autofixture.Create<int>();
+            var json = $"\"{numberId}\"";
+
+            // Act
+            var result = JsonConvert.DeserializeObject<RpcId>(json);
+
+            // Assert
+            result.Kind.Should().Be(RpcIdKind.String);
+            result.Value.Should().Be(numberId.ToString());
+        }
+
+        [Fact]
+        public void When_writing_id_and_is_a_number_then_write_correctly()
+        {
+            var id = Autofixture.Create<int>();
+            var rpcId = new RpcId
+            {
+                Kind = RpcIdKind.Number,
+                Value = id.ToString()
+            };
+
+            // Act
+            var result = JsonConvert.SerializeObject(rpcId);
+
+            // Assert
+            result.Should().Be($"{id}");
+        }
+
+        [Fact]
+        public void When_writing_id_and_is_a_string_then_write_correctly()
+        {
+            var id = Autofixture.Create<int>();
+            var rpcId = new RpcId
+            {
+                Kind = RpcIdKind.String,
+                Value = id.ToString()
+            };
+
+            // Act
+            var result = JsonConvert.SerializeObject(rpcId);
+
+            // Assert
+            result.Should().Be($"\"{id}\"");
+        }
+    }
+}
