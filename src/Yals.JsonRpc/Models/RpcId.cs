d7b232c48a5c85dd12f460bab215667230513537
Mark Lopez <m@silvenga.com>
2017-08-12 19:36:11 -0500

    Added equality to RpcId.

--- src/Yals.JsonRpc/Models/RpcId.cs
+++ src/Yals.JsonRpc/Models/RpcId.cs
@@ -16,6 +16,40 @@ namespace Yals.JsonRpc.Models
             Value = value;
             Kind = kind;
         }
+
+        protected bool Equals(RpcId other)
+        {
+            return string.Equals(Value, other.Value) && Kind == other.Kind;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((RpcId) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Value != null ? Value.GetHashCode() : 0) * 397) ^ (int) Kind;
+            }
+        }
+
+        public static bool operator ==(RpcId left, RpcId right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(RpcId left, RpcId right)
+        {
+            return !Equals(left, right);
+        }
     }
 
     public enum RpcIdKind
